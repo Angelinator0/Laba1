ff080fc960491076aadb27ca35a370832af3f24f
ANGELINIUM\Angelinator <voyeur008@gmail.com>
2025-04-04 07:28:02 +0300

    д

--- Program.cs
+++ Program.cs
@@ -1,22 +1,28 @@
 var app = Microsoft.AspNetCore.Builder.WebApplication.Create();
 
-app.MapGet("/", async (Microsoft.AspNetCore.Http.HttpContext context) =>
+//Главная страница
+app.MapGet("/", async (Microsoft.AspNetCore.Http.HttpContext context) => 
 {
+    // Устанавливаем тип контента ответа, чтобы браузер корректно отобразил HTML и поддержал кириллицу
     context.Response.ContentType = "text/html; charset=utf-8";
+    // Асинхронно отправляем HTML-код на клиент
     await context.Response.WriteAsync("<h1>Добро пожаловать в Minimal API!</h1>");
 });
 
+// Вторая страница
 app.MapGet("/about", async (Microsoft.AspNetCore.Http.HttpContext context) =>
 {
     context.Response.ContentType = "text/html; charset=utf-8";
     await context.Response.WriteAsync("<h1>О нас</h1><p>Это простая страница 'О нас'.</p>");
 });
 
+// Третья страница
 app.MapGet("/contact", async (Microsoft.AspNetCore.Http.HttpContext context) =>
 {
     context.Response.ContentType = "text/html; charset=utf-8";
     await context.Response.WriteAsync("<h1>Контакты</h1><p>Свяжитесь с нами: email@example.com</p>");
 });
 
+// Запуск приложения
 app.Run();
 
